03be7ac36b9bcfe7963ed97adf28b65eed17ca2d
DrBuzz <omenyzer@gmail.com>
2019-12-19 15:38:13 +0700

    phase 8

--- app/Facades/TestBasicServiceFacade.php
+++ app/Facades/TestBasicServiceFacade.php
@@ -3,10 +3,22 @@ namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 use App\Services\TestBasicService;
 
+/**
+ * @method static  getOne()
+ * @method static getTypes()
+ * @method static getName()
+ * @method static getAge()
+ * @method static getSex()
+ * @method static setName($name)
+ * @method static setAge($age)
+ * @method static setSex($sex)
+ * @see \App\Services\TestBasicService
+  */
+
 class TestBasicServiceFacade  extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'service.vk_basic';
+        return 'service.test_basic';
     }
 }
